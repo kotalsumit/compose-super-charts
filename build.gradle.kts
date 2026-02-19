5260b17d7d5c51ed838ee7661bb5f52819f9bfdb
Sumit Kotal <kotalsumit@gmail.com>
2026-02-19 10:00:00 +0530

    Initial project setup for the Kotlin Multiplatform charting library

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    kotlin("multiplatform").version("1.9.22").apply(false)
+    id("com.android.application").version("8.2.0").apply(false)
+    id("com.android.library").version("8.2.0").apply(false)
+    id("org.jetbrains.compose").version("1.6.0").apply(false)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "compose-super-charts"
+include(":compose-super-charts")
+include(":sample-desktop")
+include(":sample-app")
